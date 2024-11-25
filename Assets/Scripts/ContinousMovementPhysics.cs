f2b042d0665cd2419160b204ae17dab927e2181f
Shubham <shubham170905@gmail.com>
2024-11-25 20:16:53 +0530

    Initial Setup
    Added Physics Rig and Physical Interaction

---
+++ Assets/Scripts/ContinousMovementPhysics.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ContinousMovementPhysics : MonoBehaviour
+{
+    public InputActionProperty moveInputSource;
+    public InputActionProperty turnInputSource;
+
+    public LayerMask groundLayer;
+    public Rigidbody rb;
+
+    public float speed = 1;
+    public float turnSpeed = 60f;
+
+    public Transform directionSource;
+    public Transform turnSource;
+    public CapsuleCollider bodyCollider;
+
+    private Vector2 inputMoveAxis;
+    private float inputTurnAxis;
+
+    private void Update()
+    {
+        inputMoveAxis = moveInputSource.action.ReadValue<Vector2>();
+        inputTurnAxis = turnInputSource.action.ReadValue<Vector2>().x;
+    }
+
+    private void FixedUpdate()
+    {
+        if(!IsGrounded())
+            return;
+
+        Quaternion raw = Quaternion.Euler(0, directionSource.eulerAngles.y, 0);
+        Vector3 direction = raw * new Vector3(inputMoveAxis.x, 0, inputMoveAxis.y);
+
+        Vector3 targetMovePosition =  (rb.position + direction * Time.fixedDeltaTime * speed);
+
+        float angle = turnSpeed * Time.fixedDeltaTime * inputTurnAxis;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.up);
+
+        rb.MoveRotation(rb.rotation * q);
+
+        Vector3 newPosition = q * (targetMovePosition - turnSource.position) + turnSource.position;
+
+        rb.MovePosition(newPosition);
+    }
+
+    public bool IsGrounded()
+    {
+        Vector3 start = bodyCollider.transform.TransformPoint(bodyCollider.center);
+        float rayLength = bodyCollider.height / 2 - bodyCollider.radius + 0.05f;
+
+        bool hasHit = Physics.SphereCast(start, bodyCollider.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasHit;
+    }
+}
---
+++ Assets/Scripts/PhysicsRig.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsRig : MonoBehaviour
+{
+    public Transform playerHead;
+    public Transform leftController;
+    public Transform rightController;
+
+    public ConfigurableJoint leftHandJoint;
+    public ConfigurableJoint rightHandJoint;
+    public ConfigurableJoint headJoint;
+
+    public Rigidbody leftHandJointRB;
+    public Rigidbody rightHandJointRB;
+
+    public CapsuleCollider bodyColliser;
+
+    public float minHeight;
+    public float maxHeight;
+
+    private void FixedUpdate()
+    {
+        bodyColliser.height = Mathf.Clamp(playerHead.localPosition.y, minHeight, maxHeight);
+        bodyColliser.center = new Vector3 (playerHead.localPosition.x, bodyColliser.height/2, playerHead.localPosition.z);
+
+        leftHandJoint.targetPosition = leftController.localPosition;
+        leftHandJoint.targetRotation = leftController.localRotation;
+
+        rightHandJoint.targetPosition = rightController.localPosition;
+        rightHandJoint.targetRotation = rightController.localRotation;
+
+        headJoint.targetPosition = playerHead.localPosition;
+    }
+}
