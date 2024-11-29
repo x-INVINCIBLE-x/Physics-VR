0d1a6318c914092b427800450989e168429eb9f7
Shubham <shubham170905@gmail.com>
2024-11-29 23:04:22 +0530

    Added: Socket Interaction

---
+++ Assets/Scripts/XRSocketTagInteractor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public enum TargetTag
+{
+    EnergyKey
+}
+
+public class XRSocketTagInteractor : XRSocketInteractor
+{
+    public TargetTag targetTag;
+
+    public override bool CanHover(IXRHoverInteractable interactable)
+    {
+        return base.CanHover(interactable) && interactable.transform.tag == targetTag.ToString();
+    }
+
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return base.CanSelect(interactable) && interactable.transform.tag == targetTag.ToString();
+    }
+}
