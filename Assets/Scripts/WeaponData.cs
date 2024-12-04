63cb90031ce98570b6e09d757138fee2b93bb7d2
Shubham <shubham170905@gmail.com>
2024-12-04 07:48:02 +0530

    Adding: Inventory with double models

---
+++ Assets/Scripts/WeaponData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponData : ScriptableObject
+{
+    public GameObject inGameModel;
+    public GameObject inventoryModel;
+}
